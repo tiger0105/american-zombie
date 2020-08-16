0c712ad7d2912b88e43728303fd322d02ec6c26d
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2020-08-16 12:22:52 -0400

    _RagdollManager

---
+++ Assets/_RagdollManager/Scripts/Ragdoll/BodyColliderScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MLSpace
+{
+    public class BodyColliderScript : ColliderScript
+    {
+        public bool critical = false;                   // you can apply additional damage if critial
+        public BodyParts bodyPart = BodyParts.None;    // collider body part
+        public int index = -1;                          // index of collider
+
+        [SerializeField, HideInInspector]
+        private RagdollManager m_ParentRagdollManager;  // reference to parents ragdollmanager script
+
+        public void SetParentRagdollManager(RagdollManager rm)
+        {
+            m_ParentRagdollManager = rm;
+        }
+
+        public RagdollManager ParentRagdollManager
+        {
+            get { return m_ParentRagdollManager; }
+        }
+    }
+}
---
+++ Assets/_RagdollManager/Scripts/TEST/RagdollTest.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using MLSpace;
+
+public class RagdollTest : MonoBehaviour
+{
+	private RagdollManagerHum m_Ragdoll;
+
+	private float m_HitForce = 21f;
+
+	private bool m_DoubleClick = false;
+	private float m_DoubleClickMaxTime = 0.25f;
+	private float m_DoubleClickCurTime = 0.0f;
+	private int clickCount = 0;
+	private Ray currentRay;
+	public Ray CurrentRay
+	{
+		get { return currentRay; }
+		set { currentRay = value; }
+	}
+
+	void Start()
+	{
+		m_Ragdoll = GetComponent<RagdollManagerHum>();
+
+		Animator anim = GetComponent<Animator>();
+
+		m_Ragdoll.OnHit = () =>
+		{
+			anim.applyRootMotion = false;
+		};
+		m_Ragdoll.LastEvent = () =>
+		{
+			anim.applyRootMotion = true;
+		};
+
+
+		m_Ragdoll.RagdollEventTime = 3.0f;
+		m_Ragdoll.OnTimeEnd = () =>
+		{
+			m_Ragdoll.BlendToMecanim();
+		};
+	}
+
+	public void doHitReaction()
+	{
+		int mask = LayerMask.GetMask("ColliderLayer", "ColliderInactiveLayer");
+		RaycastHit rhit;
+		if (Physics.Raycast(currentRay, out rhit, 120.0f, mask))
+		{
+			BodyColliderScript bcs = rhit.collider.GetComponent<BodyColliderScript>();
+			int[] parts = new int[] { bcs.index };
+			m_Ragdoll.StartHitReaction(parts, currentRay.direction * m_HitForce);
+		}
+	}
+
+	public void doRagdoll()
+	{
+		int mask = LayerMask.GetMask("ColliderLayer", "ColliderInactiveLayer");
+		RaycastHit rhit;
+		if (Physics.Raycast(currentRay, out rhit, 120.0f, mask))
+		{
+			BodyColliderScript bcs = rhit.collider.GetComponent<BodyColliderScript>();
+			int[] parts = new int[] { bcs.index };
+			m_Ragdoll.StartRagdoll(parts, currentRay.direction * m_HitForce);
+		}
+	}
+}
